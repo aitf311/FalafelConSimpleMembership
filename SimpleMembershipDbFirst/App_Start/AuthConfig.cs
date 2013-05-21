21a4c9c08065a763e40315abccd1c0293c7610e3
Josh Eastburn <josh@falafel.com>
2013-05-20 23:12:17 -0400

    Initial code commit

---
+++ SimpleMembershipDbFirst/App_Start/AuthConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using SimpleMembershipDbFirst.Models;
+
+namespace SimpleMembershipDbFirst
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            // https://manage.dev.live.com
+            OAuthWebSecurity.RegisterMicrosoftClient(
+                clientId: "00000000480F3528",
+                clientSecret: "CAogwwIeS3jcJLbb-1GtNT2nzGfqWYMx");
+
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            //OAuthWebSecurity.RegisterFacebookClient(
+            //    appId: "",
+            //    appSecret: "");
+
+            OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
